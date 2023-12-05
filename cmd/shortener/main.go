285f641f2b64ec0f0a58c956ef74940eb1ef694c
Ilya Zakharov <zakharovvilya@yandex.ru>
2023-12-05 22:43:14 +0300

    iter1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,64 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+)
+
+const host = "localhost:8080"
+
+var urlStorage = make(map[string]string)
+
+func mainRoute(res http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodGet {
+		getHandler(res, req)
+	} else if req.Method == http.MethodPost {
+		postHandler(res, req)
+	} else {
+		res.WriteHeader(http.StatusBadRequest)
+	}
+}
+
+func getHandler(res http.ResponseWriter, req *http.Request) {
+	if value, ok := urlStorage[req.URL.Path]; ok {
+		fmt.Println(value)
+		http.Redirect(res, req, value, http.StatusTemporaryRedirect)
+		return
+	}
+	res.WriteHeader(http.StatusBadRequest)
+}
+
+func postHandler(res http.ResponseWriter, req *http.Request) {
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+		res.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	url := string(body)
+	for _, s2 := range urlStorage {
+		if s2 == url {
+			res.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	}
+	shortUrl := getShortUrl()
+	urlStorage[shortUrl] = url
+	res.WriteHeader(http.StatusCreated)
+	res.Write([]byte(fmt.Sprintf("http://%s%s", host, shortUrl)))
+}
+
+func getShortUrl() string {
+	return "/url" + strconv.Itoa(len(urlStorage)+1)
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/`, mainRoute)
+
+	err := http.ListenAndServe(host, mux)
+	if err != nil {
+		panic(err)
+	}
+}
