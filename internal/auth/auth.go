22dbc337e533449bab74c847a787f4e961676824
Ilya Zakharov <zakharovvilya@yandex.ru>
2024-02-09 00:11:23 +0400

    fix test, fix imports

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -2,11 +2,11 @@ package auth
 
 import (
 	"fmt"
-	"github.com/google/uuid"
 	"net/http"
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
+	"github.com/google/uuid"
 )
 
 type Claims struct {
--- internal/storage/db.go
+++ internal/storage/db.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+
 	_ "github.com/lib/pq"
 )
 
